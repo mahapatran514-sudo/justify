ee184f95792c5f3f15e87431326a68edb188fa61
mahapatran514-sudo <mahapatran514@gmail.com>
2026-01-13 10:07:33 +0000

    env setup added

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from db import get_conn
 from routes import bp
 
 def create_app():
@@ -7,6 +8,8 @@ def create_app():
     return app
 
 app = create_app()
+
+myconnection=get_conn()
 
 
 if __name__ == "__main__":
